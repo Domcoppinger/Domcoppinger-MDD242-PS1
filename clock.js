8c5bcca67913f04f5245c3583658886e7bab50aa
Domcoppinger <domcoppinger@googlemail.com>
2017-03-27 05:07:01 +1300

    Remade the clock design and code
    Remade the clock as an iteration of Maeda's 8th clock which, whilst
    still an updated design, is a much closer recreation of the original.
    
    This version is a slightly modernised version of Maeda's clock as shown
    through the clean, unpixelated, numbers. It also takes the changing of
    size and shade and matches these up to 'ticking' of each second. This is
    used as a non-written method of showing the passage of seconds as most
    clocks do when they 'tick'.

--- clock.js
+++ clock.js
@@ -2,44 +2,64 @@
  * use p5.js to draw a clock on a 960x500 canvas
  */ 
  
-// Concept. Have the second minute and hour displayed and then have their size be made relative to their distance from them resetting (i.e. them completing their full cycle through to their individual maximum values).
+// REMINDER - Canvas is 960 * 500 
+ 
+// CONCEPT - Recreate Meada's 8th clock. This is a clock design that displays only hours and minutes. These values alternate between each being big and then small and for my version this occurs over the course of exactly 1 second as another, non written, method of showing the passage of seconds. The text also isn't pixelated like Maeda's was, this gives the clock a revitalised and more modern feel.
+ 
+var hoursize = 1;
+var minsize = 1;
+var initialcheck = 0; // Used to disable the code that sets the initial size of the text
  
 function draw_clock(hour, minute, second, millis, alarm) {
 	background(0); // Black background
-	strokeWeight(2); // Stroke weight to 8 pixels
-	textSize(40);
-
-	textAlign(CENTER);// Aligns the text to the center (vertically)
-	fill(255); // This is used to reset the color so all the numbers aren't colored red
+	textAlign(CENTER, CENTER); // Aligns the text to the center (vertically). Important to center text around it's 'draw' point rather than starting from it's 'draw' point.
 	
-	textSize(hour * 2 + 15); //The inclusion of the equation element in this text size is to ensure that the size of the hour text is relative to the other text elements.
-	text(hour, 350, 250);
 	
-	textSize(minute + 15);	
-	fill(200);
-	text(minute, 451.5, 251.5);
-	fill(255);
-	text(minute, 450, 250);
+	// This code simply checks, on load and only once, if the second time is even or odd. It then sets the text sizes appropriately.
+	if (initialcheck == 0) {
+		if ((second % 2) == 0) { // 'True' this is an even number
+			hoursize = 280;
+			minsize = 100;
+			initialcheck = 1;
+		} else {
+			hoursize = 100;
+			minsize = 280;
+			initialcheck = 1;
+			}
+	}
 	
-	textSize(second + 15);
-	fill(200);
-	text(second, 551.5, 251.5);
-	fill(255);
-	text(second, 550, 250);
+	// Below we continually check if the seconds value is currently even or odd. This is used to determine if the hour or the minute should be growing and vice versa
+	if ((second % 2) == 0) { // 'True' this is an even number
+			if (hoursize < 280) {		// Ensures the text isn't at it's maximum size
+				hoursize = hoursize + 3; // Increase the hour text size by 3. This increment of 3 is used for all size changes and is based on the canvas FPS of 60 to ensure text goes through the full range of sizes, from 100 to 280
+			}
+			if (minsize > 100) { 		// Ensures the text isn't at it's minimum size
+				minsize = minsize - 3; // Decrease the min text size
+			}
+	}	else { // This is the exact opposite of the above code snippet for one the seconds value is odd
+			if (hoursize > 100) {
+				hoursize = hoursize - 3;
+			}
+			if (minsize < 280) {
+				minsize = minsize + 3;
+			}
+	}
 	
-	// scale(1.5); - This was based around the idea that a 'cheat' way of creating the text pixelation would be to decrease the text size and then scale the text upwards causing pixelation.
-	if (millis == 0) { // This is required because when millis == 0 we cannot divide it's text size so it keeps its last text size. This leads to a 'juttering' due to the large size of the single digit integer.
-		textSize(1);
-		}	else {
-				textSize(millis / 100);
-		}
-	fill(100);
-	text(millis, 570.5 + second / 2, 250.5); //This sets the position of the millisecond text so that it is always just beside the second value, no matter how small it is.
-		
-	fill('red'); //This is used to set the milli seconds text to red
-	text(millis, 570 + second / 2, 250); //This sets the position of the millisecond text so that it is always just beside the second value, no matter how small it is.
 	
-	console.log(hour);
-	console.log(minute);
-	console.log(second);
+	fill(hoursize - 25); // This uses the text size to also change the shade of the text. The '-25' ensures this change both maximises the color change range whilst also staying within the maximum limit for shades (a value of 255).
+	textSize(hoursize);
+	text(hour, 330, 250);
+	
+	fill(minsize - 25);
+	textSize(minsize);
+	text(minute, 630, 250);
+	
+	// The below snippet was used to visually compare the seconds counting to ensure the animation properly conveyed the passage of seconds.
+	//fill(255, 0, 0);
+	//textSize(50);
+	//text(second, 480, 450);
+	
+	// The next two lines were used to debug values
+	//console.log(hoursize);
+	//console.log(minsize);
 }
